f68c3bca4581feb31537f15ddae5f19a3a2ae758
rvczkowski <59458392+rvczkowski@users.noreply.github.com>
2025-11-30 19:12:39 +0100

    Update constants.ts
    Signed-off-by: rvczkowski <59458392+rvczkowski@users.noreply.github.com>

--- Frontend/src/lib/scripts/constants.ts
+++ Frontend/src/lib/scripts/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_PATH = import.meta.env.VITE_API_URL || "http://pterodactyl.rvcz.pl:8080" // <- Edit this string to your own API
+export const BASE_PATH = import.meta.env.VITE_API_URL || "https://api.bot.mckr.pl" // <- Edit this string to your own API
 export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://rvcz.pl/mckrbot" // <- Edit this string to your Invite link
 
 export function get(path: string) {
